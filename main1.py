c0ab6a2c315b0212520fb2541abe8dcc023867b8
istar-altvater <nilspaltvater+github@gmail.com>
2023-02-15 14:31:15 -0700

    Testing repo pull requests

--- main1.py
+++ main1.py
@@ -3,6 +3,7 @@ def main():
     # Code for main functionality of the program goes here
     print("Hello, word!")
     print("NILS THORNBERRY")
+    print("Everyone loves Silvers")
 
 if __name__ == '__main__':
     main()
