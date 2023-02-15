d0648be06a9a3d26d4ad9f4a057c9e37d6ab5d57
CodyS <codysilvers1993@hotmail.com>
2023-02-15 16:27:33 -0500

    test change

--- main1.py
+++ main1.py
@@ -2,7 +2,7 @@
 def main():
     # Code for main functionality of the program goes here
     print("Hello, word!")
-    print("Hellow!")
+    print("Helow!")
 
 if __name__ == '__main__':
     main()
