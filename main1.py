0d9aad81b1ba53e5ecad2400fdcb0d1afbcb0d26
istar-altvater <nilspaltvater+github@gmail.com>
2023-02-15 14:16:30 -0700

    test

--- main1.py
+++ main1.py
@@ -2,7 +2,8 @@
 def main():
     # Code for main functionality of the program goes here
     print("Hello, word!")
-    print("Hello, word!")
+    print("Hello, world!")
+    print("This is quite a process for something so simple...")
 
 if __name__ == '__main__':
     main()
