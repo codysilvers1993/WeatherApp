4f479b6a8a24f0db70525fad9b94d6a6502c9c16
CodyS <codysilvers1993@hotmail.com>
2023-02-15 17:44:13 -0500

    added getter functions and set parameters, Also got API.

--- main1.py
+++ main1.py
@@ -1,12 +1,24 @@
 import requests
 
+
+# function with city_name and api_key as parameters and returns weather_data
+def get_weather_data(city_name, api_key):
+    # Retrieves only CURRENT WEATHER AND FORECAST data for a given city using the OpenWeatherMap API.
+    # Just activated private key, website says it will take a couple hours before private key is active.
+    url = "http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+    response = requests.get(url)
+    weather_data = response.json()
+    return weather_data
+
+
 def main():
-    # Code for main functionality of the program goes here
-    print("Hello, word!")
-    loc = str(input("Enter Location:"))
-    print(loc)
-    x = requests.get('http://www.yahoo.com')
-    print(x.text)
+    # defines api key and city name
+    api_key = "e90573d841c1b6685552f55de613bc6a"
+    # city name hard coded for testing but will be accessed from user input later
+    city_name = "london"
+    # calls get weather functon and prints info
+    weather_data = get_weather_data(city_name, api_key)
+    print(weather_data)
 
 if __name__ == '__main__':
     main()
