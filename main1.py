9753b178a26bc361a1b9271be102589b6352bc33
istar-altvater <nilspaltvater+github@gmail.com>
2023-02-15 14:45:16 -0700

    testing import request installation

--- main1.py
+++ main1.py
@@ -1,9 +1,11 @@
+import requests
 
 def main():
     # Code for main functionality of the program goes here
     print("Hello, word!")
     loc = str(input("Enter Location:"))
     print(loc)
+
 
 if __name__ == '__main__':
     main()
