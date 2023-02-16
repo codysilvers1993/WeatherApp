91bbe56f168c1fe96beed3b1ddc29e827d5ae200
CodyS <codysilvers1993@hotmail.com>
2023-02-15 20:20:31 -0500

    fixed window frame title issue. API WORKS! but prints data as string. needs formatting.

--- main1.py
+++ main1.py
@@ -4,9 +4,10 @@ from tkinter import Label
 
 
 # blank display generator for window
+# fixed error with name of application displaying in window frame
 def display():
     window = tk.Tk()
-    window.title = 'ClimaChat'
+    window.title('ClimaChat')
     label = Label(window, text="Welcome to ClimaChat!")
     label.pack()
     width = 600
@@ -23,7 +24,7 @@ def display():
 def get_weather_data(city_name, api_key):
     # Retrieves only CURRENT WEATHER AND FORECAST data for a given city using the OpenWeatherMap API.
     # Just activated private key, website says it will take a couple hours before private key is active.
-    url = "http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
     response = requests.get(url)
     weather_data = response.json()
     return weather_data
@@ -36,7 +37,7 @@ def main():
     api_key = "e90573d841c1b6685552f55de613bc6a"
     # city name hard coded for testing but will be accessed from user input later
     city_name = "london"
-    # calls get weather functon and prints info
+    # calls get weather function and prints info
     weather_data = get_weather_data(city_name, api_key)
     print(weather_data)
 
