3b1779862ce37d234cb0d5345896314baca20205
CodyS <codysilvers1993@hotmail.com>
2023-02-19 03:25:11 -0500

    fixed more formatting. app will display info in placeholder labels. Will need to dressup app to look more modern.

--- RefactoredWeatherApp.py
+++ RefactoredWeatherApp.py
@@ -1,9 +1,6 @@
 from tkinter import *
 import tkinter as tk
-from tkinter import ttk, messagebox
-from datetime import datetime
 import requests
-import pytz
 
 # draws main window
 mainWindow = tk.Tk()
@@ -11,15 +8,10 @@ mainWindow.title("ClimaChat")
 mainWindow.geometry("500x500")
 mainWindow.resizable(False, False)
 
-
-# test function to print API data to console
-def printWeatherData():
-    city_name = getCityName()
-    weather_data = getWeatherData(city_name)
-    print(weather_data)
+# global variable to store weather data
+weather_data = None
 
 
-# passes returned city name to openWeatherAPI to gather API weather data
 def getWeatherData(getCityName):
     api_key = "e90573d841c1b6685552f55de613bc6a"
     openWeatherAPIurl = f"http://api.openweathermap.org/data/2.5/weather?q={getCityName}&appid={api_key}"
@@ -28,10 +20,29 @@ def getWeatherData(getCityName):
     return weatherData
 
 
-# function that returns city name gathered from textfield
+# function that returns city name gathered from textfield and gets weather data
 def getCityName():
+    global weather_data
     city_name = textfield.get()
-    return city_name
+    weather_data = getWeatherData(city_name)
+
+
+def print_weather_data():
+    # access global weather data variable
+    global weather_data
+    getCityName()
+    # if city name and API keywords valid will update label fields with current weather info
+    if weather_data:
+        wind_speed = weather_data["wind"]["speed"]
+        humidity = weather_data["main"]["humidity"]
+        description = weather_data["weather"][0]["description"]
+        pressure = weather_data["main"]["pressure"]
+
+        # update labels with weather information
+        windLabel.config(text=f"{wind_speed} m/s")
+        humidityLabel.config(text=f"{humidity}%")
+        descriptionLabel.config(text=description)
+        pressureLabel.config(text=f"{pressure} hPa")
 
 
 # chatbox
@@ -41,7 +52,7 @@ label.pack(pady=10)
 textfield = tk.Entry(mainWindow, justify="center", width=17, font=("Times New Roman", 25, "bold"), bg="light blue")
 textfield.place(x=95, y=65)
 textfield.focus()
-button = tk.Button(mainWindow, text="Generate Request", font=("Arial", 14), command=printWeatherData)
+button = tk.Button(mainWindow, text="Generate Request", font=("Arial", 14), command=print_weather_data)
 button.place(x=148, y=115)
 
 # labels
@@ -56,12 +67,12 @@ pressureTitleLabel = Label(mainWindow, text="PRESSURE", font=("Times New Roman",
 pressureTitleLabel.place(x=335, y=366)
 
 # placeholder labels
-windLabel = Label(text="...", font=("Times New Roman", 20, "bold"), fg="#ee666d")
+windLabel = Label(text="...", font=("Times New Roman", 12, "bold"), fg="#ee666d")
 windLabel.place(x=10, y=400)
-humidityLabel = Label(text="...", font=("Times New Roman", 20, "bold"), fg="#ee666d")
+humidityLabel = Label(text="...", font=("Times New Roman", 12, "bold"), fg="#ee666d")
 humidityLabel.place(x=70, y=400)
-descriptionLabel = Label(text="...", font=("Times New Roman", 20, "bold"), fg="#ee666d")
+descriptionLabel = Label(text="...", font=("Times New Roman", 12, "bold"), fg="#ee666d")
 descriptionLabel.place(x=185, y=400)
-pressureLabel = Label(text="...", font=("Times New Roman", 20, "bold"), fg="#ee666d")
+pressureLabel = Label(text="...", font=("Times New Roman", 12, "bold"), fg="#ee666d")
 pressureLabel.place(x=333, y=400)
 mainWindow.mainloop()
